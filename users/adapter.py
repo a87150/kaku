2e874a63a0d72d7fbb074efa89c143f8e827231b
a87150 <a871509791@outlook.com>
2017-10-10 13:46:23 +0800

    fix

--- users/adapter.py
+++ users/adapter.py
@@ -1,8 +1,10 @@
-import re
-
 from django.core.exceptions import ValidationError
 
+import re
+
 from allauth.account.adapter import DefaultAccountAdapter
+
+from .receivers import get_ip
 
 
 class AccountAdapter(DefaultAccountAdapter):
@@ -16,7 +18,7 @@ class AccountAdapter(DefaultAccountAdapter):
 
     def save_user(self, request, user, form, commit=True):
         user = super().save_user(request, user, form, commit=False)
-        user.ip_joined = request.META.get("REMOTE_ADDR", None)
+        user.ip_joined = get_ip(request)
 
         if commit:
             user.save()
